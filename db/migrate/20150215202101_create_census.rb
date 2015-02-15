cdc997783b872d16d9a3088183793829716fbb27
Chandra Carney <chandracarney@me.com>
2015-02-15 13:32:38 -0700

    census table includes name, year, number of occurrences, and gender.

---
+++ db/migrate/20150215202101_create_census.rb
@@ -0,0 +1,12 @@
+class CreateCensus < ActiveRecord::Migration
+  def change
+    create_table :census do |t|
+      t.timestamps null: false
+
+      t.integer :year, null: false, default: ""
+      t.string  :name, null: false, default: ""
+      t.string  :gender, null: false, default: ""
+      t.string  :occurrences, null: false, default: ""
+    end
+  end
+end
