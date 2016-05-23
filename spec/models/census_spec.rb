c4e79432aa9dbd1388e97ad8efdc4b6682963c92
Chandra Carney <chandracarney@me.com>
2016-05-23 14:57:09 -0400

    Small fixes for passing specs.

--- spec/models/census_spec.rb
+++ spec/models/census_spec.rb
@@ -3,12 +3,12 @@ require "rails_helper"
 RSpec.describe Census, type: :model do
   describe "name fetching" do
     it "can fetch all information related to a census" do
-      name = Census.create!(name: "Mary", year: "2014", gender: "F", occurrences: 13)
+      name = Census.create!(name: "Chandra", year: "2014", gender: "F", occurrences: 13)
 
-      expect(name.name).to eq("Mary")
-      expect(name.year).to eq(2014)
+      expect(name.name).to eq("Chandra")
+      expect(name.year).to eq("2014")
       expect(name.gender).to eq("F")
-      expect(name.occurrences).to eq("13")
+      expect(name.occurrences).to eq(13)
     end
   end
 end
